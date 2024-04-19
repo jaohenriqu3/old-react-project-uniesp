d0505013ecc921b2bb2e40356dff8ad18c23fd00
João Henrique Medeiros de Lucena <joaohenriqu3@academicos.com>
2024-04-18 21:45:28 -0300

    Primeiro Commit

---
+++ src/components/Header/index-header.tsx
@@ -0,0 +1,9 @@
+import 'style-header.css'; 
+
+const Header = () => {
+    return(<div className="header"> 
+    <h1 className='header-title'> React Exercises </h1>
+    </div>)
+} 
+
+export default Header; 
